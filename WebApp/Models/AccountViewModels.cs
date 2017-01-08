177c8d90492d01c5f1607c896946a3d9fbf07372
gercorr <gercorr@gmail.com>
2017-01-08 16:53:05 +0000

    cleaned up sub pages

--- WebApp/Models/AccountViewModels.cs
+++ WebApp/Models/AccountViewModels.cs
@@ -65,21 +65,21 @@ namespace WebApp.Models
     {
 
         [Required]
-        [Display(Name = "Username")]
+        [Display(Name = "Username *")]
         public string Username { get; set; }
         
         [EmailAddress]
-        [Display(Name = "Email (Not Required)")]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Password *")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Retype password *")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
